93ba996cf52e15dd40c54c94870271808a60dede
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2023-07-05 17:59:55 -0300

    build: wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,18 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import { SITE_BASE, SITE_URL } from './src/data/config';
 
+import relativeLinks from "astro-relative-links";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), relativeLinks()],
   site: SITE_URL,
   base: SITE_BASE,
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
       theme: 'material-theme',
-      wrap: false,
-    },
-  },
+      wrap: false
+    }
+  }
 });
--- src/data/config.ts
+++ src/data/config.ts
@@ -1,4 +1,2 @@
-const isDevelopment = process.env.NODE_ENV === 'development';
-
 export const SITE_URL = 'https://gustavod5.github.io';
-export const SITE_BASE = isDevelopment ? '/' : '/garden';
+export const SITE_BASE = '/garden';
