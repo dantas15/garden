9b1a32ee875b5c9c06ebb517d2d82404c4ebac2d
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2023-07-05 16:42:20 -0300

    build: add configuration to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import { SITE_URL } from "./src/data/config";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import { SITE_BASE, SITE_URL } from './src/data/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: SITE_URL,
+  base: SITE_BASE,
   markdown: {
-    syntaxHighlight: "shiki",
+    syntaxHighlight: 'shiki',
     shikiConfig: {
-      theme: "material-theme",
+      theme: 'material-theme',
       wrap: false,
     },
   },
--- src/data/config.ts
+++ src/data/config.ts
@@ -1,1 +1,4 @@
-export const SITE_URL = 'https://gustavod5.github.io/';
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+export const SITE_URL = 'https://gustavod5.github.io';
+export const SITE_BASE = isDevelopment ? '/' : '/garden';
