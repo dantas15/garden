a9f2b993f2b868fef8bffa30eeb2d8ca9497c6dc
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2023-07-05 15:48:20 -0300

    feat(fonts): change default fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,16 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+        sans: ["'Merriweather', sans-serif", ...defaultTheme.fontFamily.sans],
+        mono: [
+          "'JetBrains Mono Variable', sans-serif;",
+          ...defaultTheme.fontFamily.mono,
+        ],
       },
     },
   },
